e55c4b24d33eb54f44f512d6bbb8c912d42e57e3
Jarkko Lehtoranta <devel@jlranta.com>
2017-06-19 01:47:32 +0300

    LDAP: Change disabled attribute instead of property in the wizard
    This is needed for the ldap_tls disabled CSS selector to work.

--- apps/user_ldap/js/wizard/wizardTabGeneric.js
+++ apps/user_ldap/js/wizard/wizardTabGeneric.js
@@ -253,7 +253,7 @@ OCA = OCA || {};
 				$element.button("enable");
 			}
 			else if(!isMS || (isMS && hasOptions)) {
-				$element.prop('disabled', false);
+				$element.attr('disabled', false);
 			}
 		},
 
@@ -268,7 +268,7 @@ OCA = OCA || {};
 			} else if ($element.hasClass(this.bjQuiButtonClass)) {
 				$element.button("disable");
 			} else {
-				$element.prop('disabled', 'disabled');
+				$element.attr('disabled', 'disabled');
 			}
 		},
 
